378f127fdcbf79dd8bc5b84170230f0fb7939339
ibizdev <ibizdev@ibizsys.net>
2021-04-05 20:05:17 +0800

    xignzi006 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/SystemRuntime.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/SystemRuntime.java
@@ -2,6 +2,7 @@ package cn.ibizlab.pms.core.runtime;
 
 import net.ibizsys.model.IPSDynaInstService;
 import net.ibizsys.runtime.SystemImplBase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
