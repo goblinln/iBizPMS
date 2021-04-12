a131b8d4307089d2ce5e0be357ab38bdf396ed95
ibizdev <ibizdev@ibizsys.net>
2021-04-12 18:10:28 +0800

    csd123_123 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/zentao/task-sub-task-new-view/task-sub-task-new-view-base.tsx
+++ app_Web/src/pages/zentao/task-sub-task-new-view/task-sub-task-new-view-base.tsx
@@ -83,6 +83,20 @@ export class TaskSubTaskNewViewBase extends GridViewBase {
         }
     };
 
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof TaskSubTaskNewViewBase
+	 */
+    protected customViewParams: any = {
+        'name': {
+            isRawValue: true,
+            value: 'null',
+        }
+    };
+
     /**
      * 视图模型数据
      *
