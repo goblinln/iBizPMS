82854b2bb4c284859dbb5b34a17cff1144117d1d
misaka <huhai@lab.ibiz5.com>
2021-04-20 10:04:27 +0800

    fix 排序

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/SystemDataEntityRuntime.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/SystemDataEntityRuntime.java
@@ -3,14 +3,11 @@ package cn.ibizlab.pms.core.runtime;
 import net.ibizsys.model.dataentity.IPSDataEntity;
 import net.ibizsys.model.dataentity.action.IPSDEAction;
 import net.ibizsys.model.dataentity.defield.IPSDEField;
-import net.ibizsys.model.dataentity.defield.IPSLinkDEField;
 import net.ibizsys.model.dataentity.der.IPSDER1N;
 import net.ibizsys.model.dataentity.der.IPSDERBase;
-import net.ibizsys.runtime.dataentity.DEActionModes;
-import net.ibizsys.runtime.dataentity.DEActions;
-import net.ibizsys.runtime.dataentity.DERTypes;
-import net.ibizsys.runtime.dataentity.IDataEntityRuntime;
+import net.ibizsys.runtime.dataentity.der.DERTypes;
 import net.ibizsys.runtime.util.IEntityBase;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.util.ObjectUtils;
@@ -28,7 +25,7 @@ public abstract class SystemDataEntityRuntime extends SystemDataEntityRuntimeBas
     }
 
     @Override
-    protected void translateEntityAfterProceed(Object arg0, Object objRet, String strActionName, IPSDEAction iPSDEAction, IPSDataEntity iPSDataEntity, ProceedingJoinPoint joinPoint) throws Throwable {
+    protected void translateEntityAfterProceed(Object arg0, Object objRet, String strActionName, IPSDEAction iPSDEAction, IPSDataEntity iPSDataEntity, Object joinPoint) throws Throwable {
         IEntityBase iEntityBase = null;
         //更替参数
         if (objRet != null && objRet instanceof IEntityBase) {
@@ -52,7 +49,7 @@ public abstract class SystemDataEntityRuntime extends SystemDataEntityRuntimeBas
      * @param joinPoint
      * @throws Exception
      */
-    protected void resetEntityInvalidFKey(IEntityBase arg0, String strActionName, IPSDEAction iPSDEAction, IPSDataEntity iPSDataEntity, ProceedingJoinPoint joinPoint) throws Throwable {
+    protected void resetEntityInvalidFKey(IEntityBase arg0, String strActionName, IPSDEAction iPSDEAction, IPSDataEntity iPSDataEntity, Object joinPoint) throws Throwable {
         //查询实体的从关系
         java.util.List<IPSDERBase> psDERBases = iPSDataEntity.getMinorPSDERs();
         if (psDERBases == null) {
@@ -68,7 +65,7 @@ public abstract class SystemDataEntityRuntime extends SystemDataEntityRuntimeBas
         }
     }
 
-    protected void resetEntityInvalidFKey(IEntityBase arg0, String strActionName, IPSDEAction iPSDEAction, IPSDER1N iPSDER1N, IPSDataEntity iPSDataEntity, ProceedingJoinPoint joinPoint) throws Throwable {
+    protected void resetEntityInvalidFKey(IEntityBase arg0, String strActionName, IPSDEAction iPSDEAction, IPSDER1N iPSDER1N, IPSDataEntity iPSDataEntity, Object joinPoint) throws Throwable {
         Object objPickupValue = this.getFieldValue(arg0, iPSDER1N.getPSPickupDEField());
         if (ObjectUtils.isEmpty(objPickupValue)) {
             //无值，执行置空
@@ -81,6 +78,8 @@ public abstract class SystemDataEntityRuntime extends SystemDataEntityRuntimeBas
 
     protected String getFieldDataSetSortExp(IPSDEField iPSDEField) throws Exception {
         String fieldExp = super.getFieldDataSetSortExp(iPSDEField);
+        if(StringUtils.isBlank(fieldExp))
+            return iPSDEField.getName();
         if (fieldExp.indexOf(".") > 0){
             return fieldExp.substring(fieldExp.indexOf(".") + 1);
         }
