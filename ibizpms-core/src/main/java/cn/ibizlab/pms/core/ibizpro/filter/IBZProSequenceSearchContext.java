e07df3714ab24ef677ddc5561799406598184456
ibizdev <ibizdev@ibizsys.net>
2021-04-22 17:42:31 +0800

    ShineKOT 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibizpro/filter/IBZProSequenceSearchContext.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibizpro/filter/IBZProSequenceSearchContext.java
@@ -27,6 +27,10 @@ import cn.ibizlab.pms.core.ibizpro.domain.IBZProSequence;
 @Data
 public class IBZProSequenceSearchContext extends QueryWrapperContext<IBZProSequence> {
 
+	private Long n_curseq_eq;//[当前序列号]
+	public void setN_curseq_eq(Long n_curseq_eq) {
+        this.n_curseq_eq = n_curseq_eq;
+    }
 	private String n_ibzpro_sequencename_eq;//[业务序列表名称]
 	public void setN_ibzpro_sequencename_eq(String n_ibzpro_sequencename_eq) {
         this.n_ibzpro_sequencename_eq = n_ibzpro_sequencename_eq;
