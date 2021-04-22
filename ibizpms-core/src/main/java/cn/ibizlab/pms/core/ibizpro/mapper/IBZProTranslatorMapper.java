33650545cd88f83746bb8dafea3821aadee6301b
ibizdev <ibizdev@ibizsys.net>
2021-04-22 13:58:43 +0800

    xignzi006 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibizpro/mapper/IBZProTranslatorMapper.java
@@ -0,0 +1,65 @@
+package cn.ibizlab.pms.core.ibizpro.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.*;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import java.util.Map;
+import org.apache.ibatis.annotations.Select;
+import cn.ibizlab.pms.core.ibizpro.domain.IBZProTranslator;
+import cn.ibizlab.pms.core.ibizpro.filter.IBZProTranslatorSearchContext;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import java.io.Serializable;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.alibaba.fastjson.JSONObject;
+
+public interface IBZProTranslatorMapper extends BaseMapper<IBZProTranslator> {
+
+    Page<IBZProTranslator> searchDefault(IPage page, @Param("srf") IBZProTranslatorSearchContext context, @Param("ew") Wrapper<IBZProTranslator> wrapper);
+    @Override
+    IBZProTranslator selectById(Serializable id);
+    @Override
+    int insert(IBZProTranslator entity);
+    @Override
+    int updateById(@Param(Constants.ENTITY) IBZProTranslator entity);
+    @Override
+    int update(@Param(Constants.ENTITY) IBZProTranslator entity, @Param("ew") Wrapper<IBZProTranslator> updateWrapper);
+    @Override
+    int deleteById(Serializable id);
+    /**
+    * 自定义查询SQL
+    * @param sql
+    * @return
+    */
+    @Select("${sql}")
+    List<JSONObject> selectBySQL(@Param("sql") String sql, @Param("et")Map param);
+
+    /**
+    * 自定义更新SQL
+    * @param sql
+    * @return
+    */
+    @Update("${sql}")
+    boolean updateBySQL(@Param("sql") String sql, @Param("et")Map param);
+
+    /**
+    * 自定义插入SQL
+    * @param sql
+    * @return
+    */
+    @Insert("${sql}")
+    boolean insertBySQL(@Param("sql") String sql, @Param("et")Map param);
+
+    /**
+    * 自定义删除SQL
+    * @param sql
+    * @return
+    */
+    @Delete("${sql}")
+    boolean deleteBySQL(@Param("sql") String sql, @Param("et")Map param);
+
+}
---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibizpro/runtime/IBZProTranslatorRuntime.java
@@ -0,0 +1,275 @@
+package cn.ibizlab.pms.core.ibizpro.runtime;
+
+import cn.ibizlab.pms.core.ibizpro.domain.IBZProTranslator;
+import cn.ibizlab.pms.core.ibizpro.service.IIBZProTranslatorService;
+import cn.ibizlab.pms.core.ibizpro.filter.IBZProTranslatorSearchContext;
+import  cn.ibizlab.pms.util.filter.QueryWrapperContext;
+import com.baomidou.mybatisplus.extension.service.IService;
+import lombok.extern.slf4j.Slf4j;
+import net.ibizsys.model.dataentity.IPSDataEntity;
+import net.ibizsys.model.dataentity.action.IPSDEAction;
+import net.ibizsys.model.dataentity.defield.IPSDEField;
+import net.ibizsys.model.dataentity.der.IPSDER1N;
+import net.ibizsys.model.dataentity.der.IPSDERBase;
+import net.ibizsys.model.dataentity.ds.IPSDEDataQuery;
+import net.ibizsys.model.dataentity.ds.IPSDEDataSet;
+import net.ibizsys.model.dataentity.logic.IPSDELogic;
+import net.ibizsys.model.dataentity.wf.IPSDEWF;
+import net.ibizsys.runtime.IDynaInstRuntime;
+import cn.ibizlab.pms.util.domain.EntityBase;
+import cn.ibizlab.pms.util.errors.BadRequestAlertException;
+import cn.ibizlab.pms.util.helper.DELogicExecutor;
+import net.ibizsys.runtime.util.IEntityBase;
+import net.ibizsys.runtime.util.ISearchContextBase;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import net.ibizsys.runtime.dataentity.action.DEActions;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
+import java.io.Serializable;
+import java.util.List;
+
+
+
+@Aspect
+@Order(100)
+@Component("IBZProTranslatorRuntime")
+@Slf4j
+public class IBZProTranslatorRuntime extends cn.ibizlab.pms.core.runtime.SystemDataEntityRuntime {
+
+    @Autowired
+    IIBZProTranslatorService ibzprotranslatorService;
+
+    @Autowired
+    DELogicExecutor logicExecutor;
+
+    @Override
+    protected IEntityBase getSimpleEntity(Object o) {
+        if (o instanceof net.ibizsys.runtime.util.IEntityBase) {
+            return (IEntityBase) o;
+        } else {
+            return ibzprotranslatorService.sysGet((String) o);
+        }
+    }
+
+    @Override
+    public String getId() {
+        return "PSMODULES/ibizpro/PSDATAENTITIES/IBZProTranslator.json";
+    }
+
+    @Override
+    public String getName() {
+        return "IBZPRO_TRANSLATOR";
+    }
+
+    @Override
+    public IEntityBase createEntity() {
+        return new IBZProTranslator();
+    }
+
+    @Override
+    protected IService getService() {
+        return this.ibzprotranslatorService;
+    }
+
+    @Override
+    public IBZProTranslatorSearchContext createSearchContext() {
+        return new IBZProTranslatorSearchContext();
+    }
+
+    @Override
+    public boolean existsData(ISearchContextBase iSearchContextBase) {
+        Page<IBZProTranslator> domains = ibzprotranslatorService.searchDefault((IBZProTranslatorSearchContext) iSearchContextBase);
+        if (domains.getSize() == 0)
+            return false;
+        return true;
+    }
+
+    @Override
+    public Page<IBZProTranslator> searchDataSet(IPSDEDataSet iPSDEDataSet, ISearchContextBase iSearchContextBase) {
+        IBZProTranslatorSearchContext searchContext = (IBZProTranslatorSearchContext) iSearchContextBase;
+        if (iPSDEDataSet.getName().equals("DEFAULT"))
+            return ibzprotranslatorService.searchDefault(searchContext);    
+        return null;
+    }
+
+    @Override
+    public Page<IBZProTranslator> searchDataQuery(IPSDEDataQuery iPSDataQuery, ISearchContextBase iSearchContextBase) {
+        //暂未实现
+        return null;
+    }
+
+    @Override
+    public IBZProTranslator selectOne(ISearchContextBase iSearchContextBase) {
+        //单条数据查询，多条数数据时 返回第一条
+        IBZProTranslatorSearchContext searchContext = (IBZProTranslatorSearchContext) iSearchContextBase;
+        searchContext.setSize(1);
+        Page<IBZProTranslator> domains = ibzprotranslatorService.searchDefault(searchContext);
+        if (domains.getTotalElements() == 0)
+            return null;
+        return domains.getContent().get(0);
+    }
+
+    @Override
+    public List<IBZProTranslator> select(ISearchContextBase iSearchContextBase) {
+        IBZProTranslatorSearchContext searchContext = (IBZProTranslatorSearchContext) iSearchContextBase;
+        searchContext.setSize(Integer.MAX_VALUE);
+        return ibzprotranslatorService.searchDefault(searchContext).getContent();
+    }
+
+    @Override
+    public Object executeAction(String strActionName, IPSDEAction iPSDEAction, Object[] args) throws Throwable {
+        if (iPSDEAction != null) {
+            if (iPSDEAction.getName().equals("Create")) {
+                return ibzprotranslatorService.create((IBZProTranslator) args[0]);
+            }
+            else if (iPSDEAction.getName().equals("Update")) {
+                return ibzprotranslatorService.update((IBZProTranslator) args[0]);
+            }
+            else if (iPSDEAction.getName().equals("Remove")) {
+                return ibzprotranslatorService.remove((String) args[0]);
+            }
+            else if (iPSDEAction.getName().equals("Get")) {
+                if(args[0] instanceof IBZProTranslator){
+                    IBZProTranslator arg = (IBZProTranslator) args[0] ;
+                    arg = ibzprotranslatorService.get(arg.getIbzprotranslatorid()) ;
+                    return arg;
+                }else{
+                    return ibzprotranslatorService.get((String) args[0]);
+                }
+            }
+            else if (iPSDEAction.getName().equals("GetDraft")) {
+                return ibzprotranslatorService.getDraft((IBZProTranslator) args[0]);
+            }
+            else if (iPSDEAction.getName().equals("CheckKey")) {
+                return ibzprotranslatorService.checkKey((IBZProTranslator) args[0]);
+            }
+            else if (iPSDEAction.getName().equals("Save")) {
+                return ibzprotranslatorService.save((IBZProTranslator) args[0]);
+            }
+        }else if (StringUtils.isNotBlank(strActionName)) {
+            if (strActionName.equals(DEActions.CREATE)) {
+                return ibzprotranslatorService.create((IBZProTranslator) args[0]);
+            } else if (strActionName.equals(DEActions.UPDATE)) {
+                return ibzprotranslatorService.update((IBZProTranslator) args[0]);
+            } else if (strActionName.equals(DEActions.GET)) {
+                if(args[0] instanceof IBZProTranslator){
+                    IBZProTranslator arg = (IBZProTranslator) args[0] ;
+                    arg = ibzprotranslatorService.get(arg.getIbzprotranslatorid()) ;
+                    return arg;
+                }else{
+                    return ibzprotranslatorService.get((String) args[0]);
+                }
+            } else if (strActionName.equals(DEActions.REMOVE)) {
+                return ibzprotranslatorService.remove((String) args[0]);
+            } else if (strActionName.equals(DEActions.SYSGET)) {
+                return ibzprotranslatorService.sysGet((String) args[0]);
+            }  else if (strActionName.equals(DEActions.SYSUPDATE)) {
+                
+            }         }
+        
+        return null;
+    }
+
+
+    /**
+     * 执行实体处理逻辑
+     * @param arg0 实体
+     * @param iPSDEAction 行为
+     * @param iPSDELogic  逻辑
+     * @param iDynaInstRuntime 动态实例
+     * @param joinPoint
+     * @throws Throwable
+     */
+    @Override
+    protected void onExecuteDELogic(Object arg0, IPSDEAction iPSDEAction, IPSDELogic iPSDELogic, IDynaInstRuntime iDynaInstRuntime, Object joinPoint) throws Throwable {
+        if (arg0 instanceof EntityBase)
+            logicExecutor.executeLogic((EntityBase) arg0, iPSDEAction, iPSDELogic, iDynaInstRuntime);
+        else
+            throw new BadRequestAlertException(String.format("执行实体逻辑异常，不支持参数[%s]", arg0.toString()), "", "");
+    }
+
+    /**
+     * 执行实体附加逻辑
+     * @param arg0 实体
+     * @param iPSDEAction 行为
+     * @param strAttachMode 附加模式
+     * @param iDynaInstRuntime 动态实例
+     * @param joinPoint
+     * @throws Throwable
+     */
+    @Override
+    protected void onExecuteActionLogics(Object arg0, IPSDEAction iPSDEAction, String strAttachMode, IDynaInstRuntime iDynaInstRuntime, Object joinPoint) throws Throwable {
+        IBZProTranslator entity = null;
+        String action = iPSDEAction.getName();
+        if (arg0 instanceof IBZProTranslator) {
+            entity = (IBZProTranslator) arg0;
+        } else {
+            if (arg0 != null && "get".equalsIgnoreCase(action)) {
+                entity = ibzprotranslatorService.sysGet((String) arg0);
+            } else if ("remove".equalsIgnoreCase(action)) {
+                entity = new IBZProTranslator();
+                entity.setIbzprotranslatorid((String) arg0);
+            }
+        }
+        if (entity != null) {
+            logicExecutor.executeAttachLogic(entity, iPSDEAction, strAttachMode, iDynaInstRuntime);
+        } else
+            throw new BadRequestAlertException(String.format("执行实体行为附加逻辑[%s:%s]发生异常，无法获取传入参数", action, strAttachMode), "", "onExecuteActionLogics");
+    }
+
+    @Override
+    public boolean containsForeignKey(IPSDEField iPSDEField, Object objKey, IPSDERBase iPSDERBase) {
+        return false;
+    }
+
+    @Override
+    public void resetByForeignKey(IPSDEField iPSDEField, Object objKey, IPSDERBase iPSDERBase) {
+    }
+
+    @Override
+    public void removeByForeignKey(IPSDEField iPSDEField, Object objKey, IPSDERBase iPSDERBase) {
+    }
+
+    @Around("execution(* cn.ibizlab.pms.core.ibizpro.service.impl.IBZProTranslatorServiceImpl.*(..))")
+    @Transactional
+    public Object aroundMethod(ProceedingJoinPoint point) throws Throwable {
+        if (!this.isRtmodel()) {
+            return point.proceed();
+        }
+        String action = point.getSignature().getName();
+        if (action.equals("create")) {
+            return aroundAction("Create", point);
+        }
+        else if (action.equals("update")) {
+            return aroundAction("Update", point);
+        }
+        else if (action.equals("remove")) {
+            return aroundAction("Remove", point);
+        }
+        else if (action.equals("get")) {
+            return aroundAction("Get", point);
+        }
+        else if (action.equals("getDraft")) {
+            return aroundAction("GetDraft", point);
+        }
+        else if (action.equals("checkKey")) {
+            return aroundAction("CheckKey", point);
+        }
+        else if (action.equals("save")) {
+            return aroundAction("Save", point);
+        }
+        else if (action.equals("searchDefault")) {
+            return aroundDataSet("DEFAULT", point);
+        }
+        return point.proceed();
+    }
+
+}
