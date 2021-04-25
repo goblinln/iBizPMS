5d0bb2788d7f1408f0bcdec254e45c493628903f
曹赛娣 <467539648@qq.com>
2021-04-25 19:41:34 +0800

    将TestTask和User对应helper中的方法向Ex中迁移

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/TestTaskExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/TestTaskExService.java
@@ -1,8 +1,16 @@
 package cn.ibizlab.pms.core.extensions.service;
 
+import cn.ibizlab.pms.core.util.ibizzentao.common.ChangeUtil;
+import cn.ibizlab.pms.core.zentao.domain.Action;
+import cn.ibizlab.pms.core.zentao.domain.History;
+import cn.ibizlab.pms.core.zentao.service.IActionService;
 import cn.ibizlab.pms.core.zentao.service.impl.TestTaskServiceImpl;
+import cn.ibizlab.pms.util.dict.StaticDict;
+import cn.ibizlab.pms.util.helper.CachedBeanCopier;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.zentao.domain.TestTask;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -16,9 +24,55 @@ import java.util.*;
 @Service("TestTaskExService")
 public class TestTaskExService extends TestTaskServiceImpl {
 
+    @Autowired
+    IActionService iActionService;
+
     @Override
     protected Class currentModelClass() {
         return com.baomidou.mybatisplus.core.toolkit.ReflectionKit.getSuperClassGenericType(this.getClass().getSuperclass(), 1);
+    }
+
+    @Override
+    public boolean create(TestTask et) {
+        if (!super.create(et)) {
+            return false;
+        }
+        // 创建日志
+        Action action = new Action();
+        action.setObjecttype(StaticDict.Action__object_type.TESTTASK.getValue());
+        action.setObjectid(et.getId());
+        action.setAction(StaticDict.Action__type.OPENED.getValue());
+        action.setComment("");
+        action.setExtra("");
+        iActionService.createHis(action);
+        return true;
+    }
+
+
+    @Override
+    public boolean update(TestTask et) {
+        String comment = StringUtils.isNotBlank(et.getComment()) ? et.getComment() : "";
+        TestTask old = new TestTask();
+        CachedBeanCopier.copy(get(et.getId()), old);
+
+        if(!super.update(et)) {
+            return false ;
+        }
+        List<History> changes = ChangeUtil.diff(old, et);
+        if (changes.size() > 0 || StringUtils.isNotBlank(comment)) {
+            Action action = new Action();
+            action.setObjecttype(StaticDict.Action__object_type.TESTTASK.getValue());
+            action.setObjectid(et.getId());
+            action.setAction( StaticDict.Action__type.EDITED.getValue());
+            action.setComment(comment);
+            action.setExtra("");
+            action.setActor(null);
+            if (changes.size() > 0) {
+               action.setHistorys(changes);
+            }
+            iActionService.createHis(action);
+        }
+        return true;
     }
 
     /**
--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/UserExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/UserExService.java
@@ -1,8 +1,17 @@
 package cn.ibizlab.pms.core.extensions.service;
 
+import cn.ibizlab.pms.core.ibizpro.domain.IbzproConfig;
+import cn.ibizlab.pms.core.ibizpro.service.IIbzproConfigService;
+import cn.ibizlab.pms.core.zentao.domain.Action;
+import cn.ibizlab.pms.core.zentao.service.IActionService;
 import cn.ibizlab.pms.core.zentao.service.impl.UserServiceImpl;
+import cn.ibizlab.pms.util.dict.StaticDict;
+import cn.ibizlab.pms.util.security.AuthenticationUser;
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.zentao.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -15,6 +24,12 @@ import java.util.*;
 @Primary
 @Service("UserExService")
 public class UserExService extends UserServiceImpl {
+
+    @Autowired
+    IActionService iActionService;
+
+    @Autowired
+    IIbzproConfigService iIbzproConfigService;
 
     @Override
     protected Class currentModelClass() {
@@ -30,6 +45,32 @@ public class UserExService extends UserServiceImpl {
     @Transactional
     public User syncAccount(User et) {
         return super.syncAccount(et);
+    }
+
+    public Boolean login(String username) {
+        // 创建日志
+        Action action = new Action();
+        action.setObjecttype(StaticDict.Action__object_type.USER.getValue());
+        action.setObjectid(0L);
+        action.setAction(StaticDict.Action__type.LOGIN.getValue());
+        action.setComment("");
+        action.setExtra("");
+        action.setActor(username);
+        iActionService.createHis(action);
+//        iActionService.create(StaticDict.Action__object_type.USER.getValue(), 0L, StaticDict.Action__type.LOGIN.getValue(), "", "", username, true);
+        AuthenticationUser curUser = AuthenticationUser.getAuthenticationUser();
+        return true;
+    }
+
+    public JSONObject getSettings() {
+        JSONObject  jsonObject = new JSONObject();
+        List<IbzproConfig> list = iIbzproConfigService.list(new QueryWrapper<IbzproConfig>().eq("createman", AuthenticationUser.getAuthenticationUser().getUserid()).eq("Scope", StaticDict.ConfigScope.USER.getValue()).eq("vaild", StaticDict.YesNo.ITEM_1.getValue()).orderByDesc("updatedate"));
+        if(list.size() > 0) {
+            jsonObject.put("srfmstatus", list.get(0).getManagementstatus());
+        }else {
+            jsonObject.put("srfmstatus", StaticDict.ConfigManagementstatus.PRODUCT_PROJECT.getValue());
+        }
+        return jsonObject;
     }
 }
 
