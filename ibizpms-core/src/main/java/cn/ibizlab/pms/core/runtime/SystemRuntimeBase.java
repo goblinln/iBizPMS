0375634a59add18ee5d9b6992f6b515cf44117ae
ibizdev <ibizdev@ibizsys.net>
2021-04-26 09:21:25 +0800

    xignzi006 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/SystemRuntimeBase.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/SystemRuntimeBase.java
@@ -223,7 +223,7 @@ public abstract class SystemRuntimeBase extends net.ibizsys.runtime.SystemRuntim
         AuthenticationUser curUser = AuthenticationUser.getAuthenticationUser();
         List<UAAUniResAuthority> uaaUniResAuthorities = new ArrayList<>();
         uaaUniResAuthorities.addAll(userUAAUniResAuthority);
-        if (curUser.isSuperuser())
+        if (curUser.getAdminuser() == 1)
             uaaUniResAuthorities.addAll(adminUAAUniResAuthority);
         String curSystemId = curUser.getSrfsystemid();
         if (StringUtils.isEmpty(curSystemId)) {
