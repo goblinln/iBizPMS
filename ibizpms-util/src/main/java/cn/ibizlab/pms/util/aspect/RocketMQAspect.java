469ecb9020a249bb9f0ae5a743e3cfe3f06f7cd5
ibizdev <ibizdev@ibizsys.net>
2021-04-28 10:46:15 +0800

    xknnhk 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-util/src/main/java/cn/ibizlab/pms/util/aspect/RocketMQAspect.java
+++ ibizpms-util/src/main/java/cn/ibizlab/pms/util/aspect/RocketMQAspect.java
@@ -42,10 +42,10 @@ public class RocketMQAspect
     private String topic;
 
     @Around(value = "(execution(* cn.ibizlab.pms.core.*.service.IProductService.create*(..))||execution(* cn.ibizlab.pms.core.*.service.IProductService.update*(..)))")
-    public Object productDataPush(ProceedingJoinPoint point) throws Throwable{
+    public Object productProductPush(ProceedingJoinPoint point) throws Throwable{
         Object entity = getEntity(point);
         Object result = point.proceed();
-        sendMsg(topic, "datapush", entity);
+        sendMsg(topic, "productpush", entity);
         return result;
     }
 
