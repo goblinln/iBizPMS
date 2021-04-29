7351d0a965188e2edb214b96dbbbd40c5cc96960
ibizdev <ibizdev@ibizsys.net>
2021-04-29 14:27:12 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/packages/ibiz-service/src/service/sub-task/sub-task-base.service.ts
+++ app_Web/packages/ibiz-service/src/service/sub-task/sub-task-base.service.ts
@@ -316,39 +316,39 @@ export class SubTaskBaseService extends EntityBaseService<ISubTask> {
         return res;
     }
     /**
-     * ChildTask
+     * FetchChildTask
      *
      * @param {*} [_context={}]
      * @param {*} [_data = {}]
      * @returns {Promise<HttpResponse>}
      * @memberof SubTaskService
      */
-    async ChildTask(_context: any = {}, _data: any = {}): Promise<HttpResponse> {
+    async FetchChildTask(_context: any = {}, _data: any = {}): Promise<HttpResponse> {
         if (_context.project && _context.projectmodule && _context.task && true) {
-            return this.http.post(`/projects/${_context.project}/projectmodules/${_context.projectmodule}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/projects/${_context.project}/projectmodules/${_context.projectmodule}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.product && _context.story && _context.task && true) {
-            return this.http.post(`/products/${_context.product}/stories/${_context.story}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/products/${_context.product}/stories/${_context.story}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.product && _context.productplan && _context.task && true) {
-            return this.http.post(`/products/${_context.product}/productplans/${_context.productplan}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/products/${_context.product}/productplans/${_context.productplan}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.project && _context.task && true) {
-            return this.http.post(`/projects/${_context.project}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/projects/${_context.project}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.story && _context.task && true) {
-            return this.http.post(`/stories/${_context.story}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/stories/${_context.story}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.productplan && _context.task && true) {
-            return this.http.post(`/productplans/${_context.productplan}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/productplans/${_context.productplan}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.projectmodule && _context.task && true) {
-            return this.http.post(`/projectmodules/${_context.projectmodule}/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/projectmodules/${_context.projectmodule}/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
         if (_context.task && true) {
-            return this.http.post(`/tasks/${_context.task}/subtasks/childtask`, _data);
+            return this.http.post(`/tasks/${_context.task}/subtasks/fetchchildtask`, _data);
         }
-        return this.http.post(`/subtasks/childtask`, _data);
+        return this.http.post(`/subtasks/fetchchildtask`, _data);
     }
     /**
      * GetUserConcat
