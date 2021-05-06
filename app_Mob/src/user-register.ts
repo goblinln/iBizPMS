0bcc84ec4987aeb43274a316adc77d47be221481
zhujiamin <57311383+LUCIFER-ZHU@users.noreply.github.com>
2021-05-06 16:44:47 +0800

    更新日志

--- app_Mob/src/user-register.ts
+++ app_Mob/src/user-register.ts
@@ -11,6 +11,10 @@ import appCaseList from './components/app-case-list/app-case-list.vue'
 import CombFormItem from './components/comb-form-item/comb-form-item.vue'
 import AppSelect from './components/app-mob-select-dynamic/app-mob-select-dynamic.vue'
 import appTaskTeamList from './components/app-taskteam-list/app-taskteam-list.vue'
+import appUserHeader from './components/user-header/user-header.vue'
+import appMobPicturePMS from './components/app-mob-picture-pms/app-mob-picture-pms.vue'
+import AppUpdateLogList from './components/app-update-log-list/app-update-log-list.vue'
+import AppPmsUpdateLogInfo from './components/app-pms-update-log-info/app-pms-update-log-info.vue'
 
 export const UserComponent = {
     install(v: any, opt: any) {
@@ -26,5 +30,9 @@ export const UserComponent = {
       v.component('comb-form-item', CombFormItem);
       v.component('app-mob-select-dynamic', AppSelect);
       v.component('app-taskteam-list', appTaskTeamList);
+      v.component('app-user-header', appUserHeader);
+      v.component('app-mob-picture-pms', appMobPicturePMS);
+      v.component('app-update-log-list', AppUpdateLogList);
+      v.component('app-pms-update-log-info', AppPmsUpdateLogInfo);
     }
 };
