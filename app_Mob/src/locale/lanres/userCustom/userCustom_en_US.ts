f6114639596faebda8723a5f844d1d93d3af4e83
KK <57750083+zhangkang1314@users.noreply.github.com>
2021-05-06 15:10:20 +0800

    项目级多语言

--- app_Mob/src/locale/lanres/userCustom/userCustom_en_US.ts
+++ app_Mob/src/locale/lanres/userCustom/userCustom_en_US.ts
@@ -1,5 +1,9 @@
+import commonLogic from "@/locale/logic/common/common-logic";
 // 用户自定义语言资源 英文
 export default {
-
-    
+    mywork:{
+        noCloseProduct:commonLogic.appcommonhandle("未关闭的产品",null),
+        noCloseProject:commonLogic.appcommonhandle("未关闭的项目",null),
+        deferredProjects:commonLogic.appcommonhandle("已延期项目",null),
+    },
 };
