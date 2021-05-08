dd6ea7a7ca0fdd34929b83a2003e9e73c9c35f42
Mosher <yuyang_mos@163.com>
2021-05-08 15:55:30 +0800

    增加测试报告实体生成测试单自定义接口

--- app_Web/packages/ibiz-service/src/service/test-report/test-report.service.ts
+++ app_Web/packages/ibiz-service/src/service/test-report/test-report.service.ts
@@ -34,5 +34,51 @@ export class TestReportService extends TestReportBaseService {
         }
         return ___ibz___.sc.get('TestReportService');
     }
+
+    /**
+     * GetInfoTestTaskR接口方法
+     *
+     * @param {*} [context={}]
+     * @param {*} [data={}]
+     * @param {boolean} [isloading]
+     * @returns {Promise<any>}
+     * @memberof TestReportServiceBase
+     */
+    public async GetInfoTestTaskR(context: any = {}, data: any = {}, isloading?: boolean): Promise<any> {
+        if (context.project && context.testreport) {
+            let masterData: any = {};
+            Object.assign(data, masterData);
+            context.testreport = 0;
+            data.id = 0;
+            data.project = context.project;
+            if (!data.tasks) {
+                data.tasks = context.tasks;
+            }
+            let res: any = await this.http.put(`/projects/${context.project}/testreports/${context.testreport}/getinfotesttaskr`, data, isloading);
+
+            return res;
+        }
+        if (context.product && context.testreport) {
+            let masterData: any = {};
+            Object.assign(data, masterData);
+            context.testreport = 0;
+            data.id = 0;
+            data.project = context.project;
+            if (!data.tasks) {
+                data.tasks = context.tasks;
+            }
+            let res: any = await this.http.put(`/products/${context.product}/testreports/${context.testreport}/getinfotesttaskr`, data, isloading);
+
+            return res;
+        }
+        context.testreport = 0;
+        data.id = 0;
+        data.project = context.project;
+        if (!data.tasks) {
+            data.tasks = context.tasks;
+        }
+        let res: any = this.http.put(`/testreports/${context.testreport}/getinfotesttaskr`, data, isloading);
+        return res;
+    }
 }
 export default TestReportService;
