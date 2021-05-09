8479cad3f7e875e8d1c72a50913425ca07d65f37
胡维 <huwei_0315@example.com>
2021-05-09 21:22:34 +0800

    去掉临时提交的登录地址

--- app_Web/public/environments/environment.js
+++ app_Web/public/environments/environment.js
@@ -21,5 +21,6 @@ window.Environment = {
     ExportFile: '/ibizutilpms/ztdownload',
     // 文件上传
     UploadFile: '/ibizutilpms/ztupload',
-    loginUrl: 'http://ebsx.ibizee.cn',
+    // loginUrl: 'http://ebsx.ibizee.cn',
+    loginUrl: '',
 }
