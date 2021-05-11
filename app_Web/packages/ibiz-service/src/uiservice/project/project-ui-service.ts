79a96aaa9a2b90d749a6026553d44ba1418d7285
Shine-zwj <1917095344@qq.com>
2021-05-11 09:10:47 +0800

    产品，项目概况页删除按钮的界面行为

--- app_Web/packages/ibiz-service/src/uiservice/project/project-ui-service.ts
+++ app_Web/packages/ibiz-service/src/uiservice/project/project-ui-service.ts
@@ -1,5 +1,5 @@
-// import { AppServiceBase } from 'ibiz-core';
-// import { AppCenterService } from 'ibiz-vue';
+import { AppServiceBase } from 'ibiz-core';
+import { AppCenterService } from 'ibiz-vue';
 import { ProjectUIServiceBase } from './project-ui-service-base';
 
 /**
@@ -72,26 +72,25 @@ export default class ProjectUIService extends ProjectUIServiceBase {
      * 
      * @memberof ProjectUIService
      */
-    //  protected excuteAction(uIActionTag: string, args: any[], context: any = {}, params: any = {}, $event?: any, xData?: any, actionContext?: any, srfParentDeName?: string) {
-    //     if (Object.is(uIActionTag, "ReturnEdit")) {
-    //         this.ReturnEdit(args, xData, actionContext);
-    //     } else {
-    //         super.excuteAction(uIActionTag, args, context, params, $event, xData, actionContext, srfParentDeName);
-    //     }
-    // }
+     protected excuteAction(uIActionTag: string, args: any[], context: any = {}, params: any = {}, $event?: any, xData?: any, actionContext?: any, srfParentDeName?: string) {
+        if (Object.is(uIActionTag, "ReturnEdit")) {
+            this.ReturnEdit(args, xData, actionContext);
+        } else {
+            super.excuteAction(uIActionTag, args, context, params, $event, xData, actionContext, srfParentDeName);
+        }
+    }
 
     /**
      * 退出
      * 
      * @memberof ProjectUIService
      */
-    // public async ReturnEdit(args: any[], xData: any, actionContext: any) {
-    //     const _this: any = actionContext;
-    //     AppCenterService.notifyMessage({ name: 'Project', action: 'appRefresh', data: undefined });
-    //     const appNavDataService: any = AppServiceBase.getInstance().getAppNavDataService();
-    //     const item:any = appNavDataService.historyList[appNavDataService.findHistoryIndex(_this.$route)];
-    //     _this.$tabPageExp.onClose(item);
-
-    // }
+    public async ReturnEdit(args: any[], xData: any, actionContext: any) {
+        const _this: any = actionContext;
+        AppCenterService.notifyMessage({ name: 'Project', action: 'appRefresh', data: undefined });
+        const appNavDataService: any = AppServiceBase.getInstance().getAppNavDataService();
+        const item:any = appNavDataService.historyList[appNavDataService.findHistoryIndex(_this.$route)];
+        _this.$tabPageExp.onClose(item);
+    }
 
 }
