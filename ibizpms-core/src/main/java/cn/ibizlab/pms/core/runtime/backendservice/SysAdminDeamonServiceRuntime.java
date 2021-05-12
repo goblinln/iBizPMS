65a0b7f4420422061906190dec4bc9281794c371
ibizdev <ibizdev@ibizsys.net>
2021-05-12 15:52:56 +0800

    ShineKOT 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/runtime/backendservice/SysAdminDeamonServiceRuntime.java
@@ -0,0 +1,80 @@
+package cn.ibizlab.pms.core.runtime.backendservice;
+
+import cn.ibizlab.pms.util.security.AuthenticationUser;
+import net.ibizsys.runtime.ISystemRuntime;
+import net.ibizsys.runtime.security.IUserContext;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Order(100)
+@Component("SysAdminDeamonServiceRuntime")
+public class SysAdminDeamonServiceRuntime extends net.ibizsys.runtime.backend.SysUserBackendTaskRuntimeBase {
+
+    @Autowired
+    ISystemRuntime systemRuntime;
+
+    @Override
+    public String getId() {
+        return "PSSYSBACKSERVICES/SysAdminDeamon.json";
+    }
+
+    @Override
+    public String getName() {
+        return "系统管理";
+    }
+
+    @Override
+    @Around("execution(* cn.ibizlab.pms.core.util.job.SysAdminDeamonJobHandler.execute(..))")
+    public Object aroundHandler(ProceedingJoinPoint joinPoint) throws Throwable {
+        return super.aroundHandler(joinPoint);
+    }
+
+    @Override
+    protected Object execute(String strDynaInstId, String strParam, ProceedingJoinPoint joinPoint) throws Throwable {
+        String[] arr = strDynaInstId.split("\\|");
+        String strRealDynaInstId = null;
+        if (arr.length == 2) {
+            strRealDynaInstId = arr[1];
+        }
+        createUserContext(arr[0]);
+        return super.execute(strRealDynaInstId, strParam, joinPoint);
+    }
+
+    @Override
+    public ISystemRuntime getSystemRuntime() {
+        return systemRuntime;
+    }
+
+    /**
+     * 构造系统用户上下文
+     *
+     * @return
+     */
+    protected IUserContext getUserContext() {
+        return AuthenticationUser.getAuthenticationUser();
+    }
+
+    /**
+     * @param strDCId
+     * @throws Exception
+     */
+    private void createUserContext(String strDCId) throws Exception {
+        AuthenticationUser authuserdetail = new AuthenticationUser();
+        this.getPSSysBackService().getCodeName();
+        authuserdetail.setUserid("SYSTEM");
+        authuserdetail.setUsername("SYSTEM");
+        authuserdetail.setPersonname("SYSTEM");
+        authuserdetail.setSuperuser(1);
+        authuserdetail.setSrfdcid(strDCId);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(authuserdetail, null, authuserdetail.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+}
