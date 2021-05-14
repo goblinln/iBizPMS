2b685e82e24b033c8d12d11c95e45507b18d4969
KK <57750083+zhangkang1314@users.noreply.github.com>
2021-05-14 18:07:40 +0800

    临时提交界面行为插件

--- app_Mob/src/plugin/plugin/deuiaction/fileibzdownload.ts
+++ app_Mob/src/plugin/plugin/deuiaction/fileibzdownload.ts
@@ -1,5 +1,6 @@
 
 import { IPSAppDEUIAction } from "@ibiz/dynamic-model-api";
+import { UIActionTool, Util } from "ibiz-core";
  
 /**
  * 文件下载插件类
@@ -42,7 +43,30 @@ export default class Fileibzdownload {
      * @memberof Fileibzdownload
      */
     public async execute(args: any[], context: any = {}, params: any = {}, $event?: any, xData?: any, actionContext?: any, srfParentDeName?: string) {
-         
+        let data: any = {};
+        let parentContext:any = {};
+        let parentViewParam:any = {};
+        const _this: any = actionContext;
+        const _args: any[] = Util.deepCopy(args);
+        const actionTarget: string | null = 'SINGLEKEY';
+        Object.assign(context, { file: '%file%' });
+        Object.assign(params, { id: '%file%' });
+        Object.assign(params, { title: '%title%' });
+        if(_this.context){
+            parentContext = _this.context;
+        }
+        if(_this.viewparams){
+            parentViewParam = _this.viewparams;
+        }
+        context = UIActionTool.handleContextParam(actionTarget,_args,parentContext,parentViewParam,context);
+        data = UIActionTool.handleActionParam(actionTarget,_args,parentContext,parentViewParam,params);
+        context = Object.assign({},actionContext.context,context);
+        let parentObj:any = {srfparentdename:srfParentDeName?srfParentDeName:null,srfparentkey:srfParentDeName?context[srfParentDeName.toLowerCase()]:null};
+        Object.assign(data,parentObj);
+        Object.assign(context,parentObj);
+        // 自定义实体界面行为
+        let url = "../ibizutilpms/ztdownload/" + context.file;
+        window.open(url);
     }
 
 }
