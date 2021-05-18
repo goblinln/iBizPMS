c6b3bf447e81064bb1968eecfdc621d789faca97
胡维 <huwei_0315@example.com>
2021-05-18 21:26:04 +0800

    临时提交

--- app_Web/public/environments/environment.js
+++ app_Web/public/environments/environment.js
@@ -22,6 +22,7 @@ window.Environment = {
     // 文件上传
     UploadFile: '/ibizutilpms/ztupload',
     // 统一登录页
-    //loginUrl: 'http://ebsx.ibizee.cn/#/login',
-    loginUrl: '',
+    loginUrl: 'http://ebsx.ibizee.cn/#/login',
+    // loginUrl: '',
+    protalUrl: 'http://ebsx.ibizee.cn/',
 }
