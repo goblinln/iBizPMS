553c0f05141b00e448fa00237d7af072c113089a
ibizdev <ibizdev@ibizsys.net>
2021-05-19 17:14:25 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/plugin/plugin/custom/testleftnavlist.tsx
+++ app_Web/src/plugin/plugin/custom/testleftnavlist.tsx
@@ -19,7 +19,15 @@ import { IPSDEListDataItem } from '@ibiz/dynamic-model-api';
 @VueLifeCycleProcessing()
 export class TESTLEFTNAVLIST extends AppDefaultList {
 
+   
     /**
+     * 选中项
+     *
+     * @memberof LEFTNAVLIST
+     */
+    public selectItem = '';    
+
+	/**
      * 项单机
      *
      * @protected
@@ -29,6 +37,7 @@ export class TESTLEFTNAVLIST extends AppDefaultList {
      */
     protected click(e: MouseEvent, item: Product): void {
         e.stopPropagation();
+			this.selectItem = item.id;
         this.handleDblClick(item);
     }
 
@@ -97,7 +106,7 @@ export class TESTLEFTNAVLIST extends AppDefaultList {
         let statusItem: IPSDEListDataItem | undefined =  this.controlInstance.getPSDEListDataItems()?.find((item:any)=>{
             return item.name == 'status'
         });
-        return <listItem class={{ 'is-top': p.istop }}>
+        return <listItem class={{ 'is-top': p.istop ,'is-select':p.id===this.selectItem}}>
             <div class="content-wrapper" on-click={(e: any) => this.click(e, p)}>
                 <div class="title">
                     <tag type="border">{p.id}</tag>
