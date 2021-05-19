e048fd659df1b844e662c51d192b32d5e42cb4e5
WodahsOrez <41291430+WodahsOrez@users.noreply.github.com>
2021-05-19 17:01:19 +0800

    删除自定义

--- app_Web/src/utils/custom-notice-handler/custom-notic-handler.ts
+++ app_Web/src/utils/custom-notice-handler/custom-notic-handler.ts
@@ -1,26 +1,6 @@
 import { NoticeHandler } from "ibiz-vue";
 
 /**
- * 用户没有权限时禁用所有表单成员
- *
- * @param {{ error: any, param: any }} { error, param }
- * @memberof FormControlBase
- */
-function handleAuth({ error, param, caller, fnName }: { error: any, param: any, caller: any, fnName: string }){
-    if(caller?.controlInstance?.controlType == 'FORM' && fnName == 'load'){
-        if(error?.status == 403){
-            // 禁用异常提示
-            error.ignore = true;
-            // 用户没权限禁用所有的表单成员
-            Object.keys(caller.detailsModel).forEach((key: string)=>{
-                caller.detailsModel[key].disabled = true;
-            })
-            caller.$forceUpdate();
-        }
-    }
-}
-
-/**
  * 处理备注保存成功提示信息过长。
  *
  * @param {{ message: any, param: any }} { error, param }
@@ -34,6 +14,5 @@ function handleCommentTooLong(args: { message: any, param: any, caller: any, fnN
 }
 
 export function initNoticeHandler(){
-    NoticeHandler.hooks.beforeError.tap(handleAuth)
     NoticeHandler.hooks.beforeSuccess.tap(handleCommentTooLong)
 }
