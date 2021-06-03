b077555dc9c44200bc52b586c1df3b448732b731
胡维 <huwei_0315@example.com>
2021-06-03 20:41:29 +0800

    重写User查询

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/SysUserExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/SysUserExService.java
@@ -1,8 +1,17 @@
 package cn.ibizlab.pms.core.extensions.service;
 
+import cn.ibizlab.pms.core.ibiz.domain.IbzMyTerritory;
+import cn.ibizlab.pms.core.ibiz.filter.IbzMyTerritorySearchContext;
+import cn.ibizlab.pms.core.ibiz.service.IIbzMyTerritoryService;
+import cn.ibizlab.pms.core.uaa.filter.SysUserSearchContext;
 import cn.ibizlab.pms.core.uaa.service.impl.SysUserServiceImpl;
+import cn.ibizlab.pms.util.helper.CachedBeanCopier;
+import cn.ibizlab.pms.util.security.AuthenticationUser;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.uaa.domain.SysUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -17,6 +26,9 @@ import java.util.*;
 public class SysUserExService extends SysUserServiceImpl {
 
 
+    @Autowired
+    IIbzMyTerritoryService iIbzMyTerritoryService;
+
     /**
      * [ChangePwd:修改密码] 行为扩展
      * @param et
@@ -26,6 +38,41 @@ public class SysUserExService extends SysUserServiceImpl {
     @Transactional
     public SysUser changePwd(SysUser et) {
         return super.changePwd(et);
+    }
+
+    @Override
+    public SysUser get(String userid) {
+        SysUser sysUser = new SysUser();
+        AuthenticationUser authenticationUser = AuthenticationUser.getAuthenticationUser();
+        CachedBeanCopier.copy(authenticationUser, sysUser);
+        return sysUser;
+    }
+
+    @Override
+    public Page<SysUser> searchMyWork(SysUserSearchContext context) {
+        IbzMyTerritorySearchContext searchContext = new IbzMyTerritorySearchContext();
+        searchContext.setN_account_eq(AuthenticationUser.getAuthenticationUser().getLoginname());
+        List<IbzMyTerritory> list = iIbzMyTerritoryService.selectMyWork(searchContext);
+        SysUser sysUser = get(AuthenticationUser.getAuthenticationUser().getUserid());
+        List<SysUser> userList = new ArrayList<>();
+        if(list.size() > 0) {
+            CachedBeanCopier.copy(list.get(0), sysUser);
+        }
+        userList.add(sysUser);
+        return new PageImpl<SysUser>(userList, context.getPageable(), 1);
+    }
+
+    @Override
+    public Page<SysUser> searchPersonInfo(SysUserSearchContext context) {
+        IbzMyTerritorySearchContext searchContext = new IbzMyTerritorySearchContext();
+        List<IbzMyTerritory> list = iIbzMyTerritoryService.selectPersonInfo(searchContext);
+        SysUser sysUser = get(AuthenticationUser.getAuthenticationUser().getUserid());
+        List<SysUser> userList = new ArrayList<>();
+        if(list.size() > 0) {
+            CachedBeanCopier.copy(list.get(0), sysUser);
+        }
+        userList.add(sysUser);
+        return new PageImpl<SysUser>(userList, context.getPageable(), 1);
     }
 }
 
