57bf01acfd809da17e2c1bb9cb9aa5e244b90b28
ibizdev <ibizdev@ibizsys.net>
2021-06-07 22:24:55 +0800

    huwei_0806 部署微服务接口 [后台服务体系，标准版服务接口]

--- ibizpms-provider/ibizpms-provider-standardapi/src/main/java/cn/ibizlab/pms/standardapi/rest/FileResource.java
+++ ibizpms-provider/ibizpms-provider-standardapi/src/main/java/cn/ibizlab/pms/standardapi/rest/FileResource.java
@@ -1758,65 +1758,6 @@ public class FileResource {
 
 
     @PreAuthorize("quickTest('ZT_FILE','READ')")
-	@ApiOperation(value = "根据产品版本获取文件库查询", tags = {"附件" } ,notes = "根据产品版本获取文件库查询")
-    @RequestMapping(method= RequestMethod.POST , value="/tests/{product_id}/builds/{build_id}/files/fetchproduct")
-	public ResponseEntity<List<FileDTO>> fetchProductByProductBuild(@PathVariable("product_id") Long product_id, @PathVariable("build_id") Long build_id,@RequestBody FileSearchContext context) {
-        
-        fileRuntime.addAuthorityConditions(context,"READ");
-        Page<File> domains = fileService.searchProductDocLibFile(context) ;
-        List<FileDTO> list = fileMapping.toDto(domains.getContent());
-	    return ResponseEntity.status(HttpStatus.OK)
-                .header("x-page", String.valueOf(context.getPageable().getPageNumber()))
-                .header("x-per-page", String.valueOf(context.getPageable().getPageSize()))
-                .header("x-total", String.valueOf(domains.getTotalElements()))
-                .body(list);
-	}
-    @PreAuthorize("test('ZT_FILE', #file_id, 'DELETE')")
-    @ApiOperation(value = "根据产品版本删除附件", tags = {"附件" },  notes = "根据产品版本删除附件")
-	@RequestMapping(method = RequestMethod.DELETE, value = "/tests/{product_id}/builds/{build_id}/files/{file_id}")
-    public ResponseEntity<Boolean> removeByProductBuild(@PathVariable("product_id") Long product_id, @PathVariable("build_id") Long build_id, @PathVariable("file_id") Long file_id) {
-		return ResponseEntity.status(HttpStatus.OK).body(fileService.remove(file_id));
-    }
-
-    @PreAuthorize("quickTest('ZT_FILE','DELETE')")
-    @ApiOperation(value = "根据产品版本批量删除附件", tags = {"附件" },  notes = "根据产品版本批量删除附件")
-	@RequestMapping(method = RequestMethod.DELETE, value = "/tests/{product_id}/builds/{build_id}/files/batch")
-    public ResponseEntity<Boolean> removeBatchByProductBuild(@RequestBody List<Long> ids) {
-        fileService.removeBatch(ids);
-        return  ResponseEntity.status(HttpStatus.OK).body(true);
-    }
-
-    @PreAuthorize("quickTest('ZT_FILE','READ')")
-	@ApiOperation(value = "根据产品版本获取文件库查询", tags = {"附件" } ,notes = "根据产品版本获取文件库查询")
-    @RequestMapping(method= RequestMethod.POST , value="/tests/{product_id}/builds/{build_id}/files/fetchproject")
-	public ResponseEntity<List<FileDTO>> fetchProjectByProductBuild(@PathVariable("product_id") Long product_id, @PathVariable("build_id") Long build_id,@RequestBody FileSearchContext context) {
-        
-        fileRuntime.addAuthorityConditions(context,"READ");
-        Page<File> domains = fileService.searchDocLibFile(context) ;
-        List<FileDTO> list = fileMapping.toDto(domains.getContent());
-	    return ResponseEntity.status(HttpStatus.OK)
-                .header("x-page", String.valueOf(context.getPageable().getPageNumber()))
-                .header("x-per-page", String.valueOf(context.getPageable().getPageSize()))
-                .header("x-total", String.valueOf(domains.getTotalElements()))
-                .body(list);
-	}
-    @PreAuthorize("quickTest('ZT_FILE','READ')")
-	@ApiOperation(value = "根据产品版本获取动态(根据类型过滤)", tags = {"附件" } ,notes = "根据产品版本获取动态(根据类型过滤)")
-    @RequestMapping(method= RequestMethod.POST , value="/tests/{product_id}/builds/{build_id}/files/fetchtype")
-	public ResponseEntity<List<FileDTO>> fetchTypeByProductBuild(@PathVariable("product_id") Long product_id, @PathVariable("build_id") Long build_id,@RequestBody FileSearchContext context) {
-        
-        fileRuntime.addAuthorityConditions(context,"READ");
-        Page<File> domains = fileService.searchType(context) ;
-        List<FileDTO> list = fileMapping.toDto(domains.getContent());
-	    return ResponseEntity.status(HttpStatus.OK)
-                .header("x-page", String.valueOf(context.getPageable().getPageNumber()))
-                .header("x-per-page", String.valueOf(context.getPageable().getPageSize()))
-                .header("x-total", String.valueOf(domains.getTotalElements()))
-                .body(list);
-	}
-
-
-    @PreAuthorize("quickTest('ZT_FILE','READ')")
 	@ApiOperation(value = "根据产品Bug获取文件库查询", tags = {"附件" } ,notes = "根据产品Bug获取文件库查询")
     @RequestMapping(method= RequestMethod.POST , value="/tests/{product_id}/bugs/{bug_id}/files/fetchproduct")
 	public ResponseEntity<List<FileDTO>> fetchProductByProductBug(@PathVariable("product_id") Long product_id, @PathVariable("bug_id") Long bug_id,@RequestBody FileSearchContext context) {
