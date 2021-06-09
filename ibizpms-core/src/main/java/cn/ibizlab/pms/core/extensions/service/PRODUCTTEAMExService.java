1c06cccbf076f134f7bffe39673ad7576d21e731
胡维 <huwei_0315@example.com>
2021-06-09 17:40:46 +0800

    项目团队和项目团队数据集调整

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/PRODUCTTEAMExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/PRODUCTTEAMExService.java
@@ -2,9 +2,15 @@ package cn.ibizlab.pms.core.extensions.service;
 
 import cn.ibizlab.pms.core.ibiz.filter.PRODUCTTEAMSearchContext;
 import cn.ibizlab.pms.core.ibiz.service.impl.PRODUCTTEAMServiceImpl;
+import cn.ibizlab.pms.core.ou.domain.SysEmployee;
+import cn.ibizlab.pms.core.ou.filter.SysEmployeeSearchContext;
+import cn.ibizlab.pms.core.ou.service.ISysEmployeeService;
+import cn.ibizlab.pms.util.dict.StaticDict;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.ibiz.domain.PRODUCTTEAM;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -23,6 +29,9 @@ public class PRODUCTTEAMExService extends PRODUCTTEAMServiceImpl {
         return com.baomidou.mybatisplus.core.toolkit.ReflectionKit.getSuperClassGenericType(this.getClass().getSuperclass(), 1);
     }
 
+    @Autowired
+    ISysEmployeeService iSysEmployeeService;
+
     /**
      * [ProductTeamGuoLv:PmsEe团队管理过滤] 行为扩展
      * @param et
@@ -37,10 +46,26 @@ public class PRODUCTTEAMExService extends PRODUCTTEAMServiceImpl {
     @Override
     public Page<PRODUCTTEAM> searchProductTeamInfo(PRODUCTTEAMSearchContext context) {
         Map<String, Object> params = context.getParams();
-        if(params.get("dept") != null || params.get("teams") != null) {
-            return super.searchRowEditDefaultProductTeam(context);
+        List<PRODUCTTEAM> productteams = new ArrayList<>();
+        if(params.get("teams") != null) {
+            productteams.addAll(super.selectRowEditDefaultProductTeam(context));
+        }else {
+            productteams.addAll(super.selectProductTeamInfo(context));
         }
-        return super.searchProductTeamInfo(context);
+        if(params.get("dept") != null) {
+            List<SysEmployee> list = iSysEmployeeService.selectByMdeptid(params.get("dept").toString());
+            Long root = params.get("root") != null ? Long.parseLong(params.get("root").toString()) : 0L;
+            for(SysEmployee sysEmployee : list) {
+                PRODUCTTEAM productteam = new PRODUCTTEAM();
+                productteam.setRoot(root);
+                productteam.setDays(5);
+                productteam.setHours(7D);
+                productteam.setLimited(StaticDict.YesNo.ITEM_0.getValue());
+                productteam.setAccount(sysEmployee.getLoginname());
+                productteams.add(productteam);
+            }
+        }
+        return new PageImpl<PRODUCTTEAM>(productteams, context.getPageable(), productteams.size());
     }
 }
 
--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/ProjectTeamExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/ProjectTeamExService.java
@@ -1,10 +1,16 @@
 package cn.ibizlab.pms.core.extensions.service;
 
+import cn.ibizlab.pms.core.ibiz.domain.PRODUCTTEAM;
 import cn.ibizlab.pms.core.ibiz.filter.ProjectTeamSearchContext;
 import cn.ibizlab.pms.core.ibiz.service.impl.ProjectTeamServiceImpl;
+import cn.ibizlab.pms.core.ou.domain.SysEmployee;
+import cn.ibizlab.pms.core.ou.service.ISysEmployeeService;
+import cn.ibizlab.pms.util.dict.StaticDict;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.ibiz.domain.ProjectTeam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -23,6 +29,9 @@ public class ProjectTeamExService extends ProjectTeamServiceImpl {
         return com.baomidou.mybatisplus.core.toolkit.ReflectionKit.getSuperClassGenericType(this.getClass().getSuperclass(), 1);
     }
 
+    @Autowired
+    ISysEmployeeService iSysEmployeeService;
+
     /**
      * [GetUserRole:获取成员角色] 行为扩展：根据成员获取成员的角色信息
      * @param et
@@ -37,10 +46,26 @@ public class ProjectTeamExService extends ProjectTeamServiceImpl {
     @Override
     public Page<ProjectTeam> searchTaskCntEstimateConsumedLeft(ProjectTeamSearchContext context) {
         Map<String, Object> params = context.getParams();
-        if(params.get("dept") != null || params.get("teams") != null) {
-            return super.searchRowEditDefault(context);
+        List<ProjectTeam> projectTeams = new ArrayList<>();
+        if(params.get("teams") != null) {
+            projectTeams.addAll(super.selectRowEditDefault(context));
+        }else {
+            projectTeams.addAll(super.selectTaskCntEstimateConsumedLeft(context));
         }
-        return super.searchTaskCntEstimateConsumedLeft(context);
+        if(params.get("dept") != null) {
+            List<SysEmployee> list = iSysEmployeeService.selectByMdeptid(params.get("dept").toString());
+            Long root = params.get("root") != null ? Long.parseLong(params.get("root").toString()) : 0L;
+            for(SysEmployee sysEmployee : list) {
+                ProjectTeam projectTeam = new ProjectTeam();
+                projectTeam.setRoot(root);
+                projectTeam.setDays(5);
+                projectTeam.setHours(7D);
+                projectTeam.setLimited(StaticDict.YesNo.ITEM_0.getValue());
+                projectTeam.setAccount(sysEmployee.getLoginname());
+                projectTeams.add(projectTeam);
+            }
+        }
+        return new PageImpl<ProjectTeam>(projectTeams, context.getPageable(), projectTeams.size());
     }
 }
 
