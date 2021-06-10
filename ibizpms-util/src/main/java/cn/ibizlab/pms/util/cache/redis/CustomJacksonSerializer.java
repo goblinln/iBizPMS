3a3b6e8929912b9a48dc4d5dee67dfc778e63bfb
ibizdev <ibizdev@ibizsys.net>
2021-06-10 23:21:12 +0800

    zhouweidong 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-util/src/main/java/cn/ibizlab/pms/util/cache/redis/CustomJacksonSerializer.java
+++ ibizpms-util/src/main/java/cn/ibizlab/pms/util/cache/redis/CustomJacksonSerializer.java
@@ -1,10 +1,12 @@
 package cn.ibizlab.pms.util.cache.redis;
 
-import cn.ibizlab.pms.util.security.AuthenticationUser;
+import cn.ibizlab.pms.util.security.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomJacksonSerializer<T> extends Jackson2JsonRedisSerializer<T>  {
 
@@ -15,6 +17,15 @@ public class CustomJacksonSerializer<T> extends Jackson2JsonRedisSerializer<T>  
     public static final String CLASSNAME_EX_PATTEN="(_\\$\\$_)(\\w+)";
 
     public static final String USER_PACKAGE= AuthenticationUser.class.getName();
+
+    private static HashMap<String, String> authorityClassMap = new HashMap<>();
+
+    static {
+        authorityClassMap.put("cn.ibizlab.util.security.UAAUniResAuthority", UAAUniResAuthority.class.getName());
+        authorityClassMap.put("cn.ibizlab.util.security.UAAMenuAuthority", UAAMenuAuthority.class.getName());
+        authorityClassMap.put("cn.ibizlab.util.security.UAADEAuthority", UAADEAuthority.class.getName());
+        authorityClassMap.put("cn.ibizlab.util.security.UAARoleAuthority", UAARoleAuthority.class.getName());
+    }
 
     public CustomJacksonSerializer(Class type) {
         super(type);
@@ -29,6 +40,11 @@ public class CustomJacksonSerializer<T> extends Jackson2JsonRedisSerializer<T>  
         }
         if(serializerContent.contains(CLASSNAME_EX)){
             serializerContent=serializerContent.replaceAll(CLASSNAME_EX_PATTEN,"");
+        }
+        for(Map.Entry<String, String> entry : authorityClassMap.entrySet()){
+            if(serializerContent.contains(entry.getKey())){
+                serializerContent=serializerContent.replaceAll(entry.getKey(), entry.getValue());
+            }
         }
         return super.deserialize(serializerContent.getBytes());
     }
