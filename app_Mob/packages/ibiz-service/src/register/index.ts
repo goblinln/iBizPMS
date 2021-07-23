d56de80da0ff22ac17b20bf129c16bf09cc563e6
ibizdev <ibizdev@ibizsys.net>
2021-07-23 11:48:24 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/packages/ibiz-service/src/register/index.ts
+++ app_Mob/packages/ibiz-service/src/register/index.ts
@@ -1,7 +1,6 @@
 export { UIServiceRegister } from "./ui-service-register";
 // TODO
 export { AuthServiceRegister } from "./auth-service-register";
-export { codeListRegister,CodeListRegister } from "./codelist-register";
 export { CounterServiceRegister } from "./counter-service-register";
 // todo 视图消息
 // export { messageServiceRegister,MessageServiceRegister } from "./message-service-register";
